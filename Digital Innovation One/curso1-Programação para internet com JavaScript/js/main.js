783ea9e20b169d1b9e0bc6ac91a73dd31b16a680
samaramelk <85246683+samaramelk@users.noreply.github.com>
2021-07-05 22:27:45 -0300

    Add files via upload

---
+++ Digital Innovation One/curso1-Programação para internet com JavaScript/js/main.js
@@ -0,0 +1,10 @@
+var nome = 'Samara melk';
+var idade = '29';
+var idade2 = '10';
+var frase = 'Japão é o melhor time do mundo';
+alert(nome + 'tem' + idade + 'anos');
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
+//alert(frase.replace('Japão','Brasil'));
